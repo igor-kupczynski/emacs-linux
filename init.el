1414adbdb7a7bc0ae4cf800adf6636ccf91c4298
Igor Kupczynski <puszczyk@gmail.com>
2012-10-04 08:33:02 +0200

    configured el-get

--- init.el
+++ init.el
@@ -9,24 +9,21 @@
 
 ;; Custom
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
- '(safe-local-variable-values (quote ((ispell-dictionary . "polish") (encoding . utf-8) (ispell-local-dictionary . en_GB) (ispell-local-directory . "polish") (ispell-local-dictionary . polish) (dictionary . polish)))))
+ '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (ispell-dictionary . "polish") (encoding . utf-8) (ispell-local-dictionary . en_GB) (ispell-local-directory . "polish") (ispell-local-dictionary . polish) (dictionary . polish)))))
 
 
 ;; Load path
 (add-to-list 'load-path "~/.emacs.d")
-(progn (cd "~/.emacs.d")
-       (normal-top-level-add-subdirs-to-load-path))
-(add-to-list 'load-path "~/.emacs.d/packages")
-(progn (cd "~/.emacs.d/packages")
+(progn (cd "~/.emacs.d/modules.local")
        (normal-top-level-add-subdirs-to-load-path))
 
 ;; Init packages
-;;(load-library "puszczyk-packages.el")
+(load-library "packages.el")
 
 ;; Theme
 ;;(load-library "puszczyk-theme.el")
@@ -40,3 +37,9 @@
 
 ;; Misc
 ;;(load-library "puszczyk-misc.el")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ packages.el
@@ -0,0 +1,31 @@
+;; - ac-nrepl
+;; - projectile
+;; - tramp ???
+  
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (let (el-get-master-branch)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))))
+
+(setq el-get-user-package-directory "~/.emacs.d/packages.conf")
+
+(setq el-get-sources '())
+
+(setq puszczyk-packages
+      (append
+       '(auto-complete
+	 clojure-mode
+	 color-theme-tomorrow
+	 el-get
+	 markdown-mode
+	 paredit
+	 session
+	 )
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync puszczyk-packages)
